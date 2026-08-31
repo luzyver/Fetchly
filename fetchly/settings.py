58b1955afc4b9834d65f56d8fabe58732f75f909
𝙻𝚄𝚉𝚈𝚅𝙴𝚁 <91455306+luzyver@users.noreply.github.com>
2026-09-01 04:14:36 +0700

    change to ServeStatic

--- fetchly/settings.py
+++ fetchly/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "servestatic.middleware.ServeStaticMiddleware",
     "fetchly.middleware.RequestIdMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
